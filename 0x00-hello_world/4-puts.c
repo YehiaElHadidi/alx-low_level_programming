40d2ffeb9fdf32b55f28f4b8a7132cf125ee88f2
YehiaElHadidi <hadidiyehia@gmail.com>
2023-03-16 10:18:58 -0700

    s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 
 int main(void)
 {
-	char str[] = "\"Programming is like building a multilingual puzzle";
+	char str[100] = "\"Programming is like building a multilingual puzzle";
         return (0);
 }
