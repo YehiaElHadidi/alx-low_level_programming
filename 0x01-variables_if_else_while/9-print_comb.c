129004eaa2f0fc0f5b9de8fad169a18a09b0f2e4
YehiaElHadidi <hadidiyehia@gmail.com>
2023-03-17 06:07:34 -0700

    s

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 56; i++)
+	{
+		putchar(i);
+		putchar(',');
+	}
+	putchar('9');
+	putchar('\n');
+	return (0);
+}
