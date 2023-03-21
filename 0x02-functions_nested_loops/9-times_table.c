a6b5b25ae58160784f5acd99ca815a9506fe618f
YehiaElHadidi <hadidiyehia@gmail.com>
2023-03-21 11:34:25 -0700

    s

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * times_table - void parameter
+ * Description: prints alphabet in small letters
+ *
+ * Return: void Always
+*/
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		sum = i;
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar(sum + 48);
+			if (i != 9)
+			{
+				_putchar(' ');
+				_putchar(',');
+			}
+			sum += i;
+		}
+		_putchar('\n');
+	}
+}
