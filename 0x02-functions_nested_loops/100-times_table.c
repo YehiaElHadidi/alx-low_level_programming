87f6e2d8f2394c5f0126282138baf87c930ac4d3
YehiaElHadidi <hadidiyehia@gmail.com>
2023-03-21 12:40:39 -0700

    s

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,23 +11,21 @@ void print_times_table(int n)
 {
 	int i, j;
 
+	if (n > 15)
+		return;
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
 		{
 			if (j != 0)
 			{
-				_putchar(',');
-				_putchar(' ');
+				printf(", ");
 			}
-			if (i * j <= 9 && j != 0)
-				_putchar(' ');
-
-			if (i * j > 9)
-			{
-				_putchar((((i * j) / 10) % 10) + 48);
-			}
-			_putchar(((i * j) % 10) + 48);
+			if (i * j < 100)
+				printf(" ");
+			if (i * j < 10)
+				printf(" ");
+			print(i*j)
 		}
 		_putchar('\n');
 	}
