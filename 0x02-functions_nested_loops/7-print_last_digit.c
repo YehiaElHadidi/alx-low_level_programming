1576ea43eb97f3a92a8adc0224ad797026054b68
YehiaElHadidi <hadidiyehia@gmail.com>
2023-03-21 10:57:35 -0700

    s

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * print_last_digit - takes one int parameter
+ * Description: prints alphabet in small letters
+ *
+ * @n : check n
+ * Return: int Always
+*/
+int print_last_digit(int n)
+{
+	_putchar((n % 10) + '0');
+	return ((n % 10) + '0');
+}
