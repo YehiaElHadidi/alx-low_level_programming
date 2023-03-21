4cbb38910589e457ed4406f54ff0bc721d07b43e
YehiaElHadidi <hadidiyehia@gmail.com>
2023-03-21 10:29:59 -0700

    s

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * print_alphabet - void parameters
+ * Description: prints alphabet in small letters
+ * Return: void Always
+*/
+void print_alphabet(void)
+{
+	int i, j;
+
+	for(j = 0; j < 10; j++){ 
+		for (i = 0; i < 26; i++)
+		{
+			_putchar(i + 'a');
+		}
+		_putchar('\n');
+	}
+}
