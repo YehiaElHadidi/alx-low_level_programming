59ba75ffdd912ff47add2df2a8e6324fb25fd26c
YehiaElHadidi <hadidiyehia@gmail.com>
2023-03-23 10:44:34 -0700

    s

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 
 /**
- * main - tests if input given is a digit
- * Return: 0
- */
+ * main - prints the number from 1 - 100, but for mult of 3 & 5 print Fizz Buzz
+ *
+ * Return: Always 0 (Success)
+**/
 
 int main(void)
 {
@@ -13,12 +14,20 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
+		{
 			printf("Fizz");
+		}
 		if (i % 5 == 0)
+		{
 			printf("Buzz");
+		}
 		if (i != 100)
+		{
 			printf(" ");
+		}
 		if (!(i % 3 == 0 || i % 5 == 0))
+		{
 			printf("%d", i);
+		}
 	}
 }
