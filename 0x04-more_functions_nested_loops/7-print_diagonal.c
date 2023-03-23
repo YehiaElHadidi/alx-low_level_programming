b93e7c5118c022dd0088dd5d8cb8d82da35554e4
YehiaElHadidi <hadidiyehia@gmail.com>
2023-03-23 08:33:02 -0700

    s

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagonal - tests if input given is a digit
+ * Return: void
+ * @n: int
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n - 1; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
