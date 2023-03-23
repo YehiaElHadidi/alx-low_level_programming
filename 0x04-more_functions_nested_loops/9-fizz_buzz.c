0bdece594d248e7244365ebff7d09fc97bfad07c
YehiaElHadidi <hadidiyehia@gmail.com>
2023-03-23 10:41:42 -0700

    s

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,7 +15,7 @@ int main(void)
 		if (i % 3 == 0)
 			printf("Fizz");
 		if (i % 5 == 0)
-			printf("Buzz")
+			printf("Buzz");
 		if (i != 100)
 			printf(" ");
 		if (!(i % 3 == 0 || i % 5 == 0))
