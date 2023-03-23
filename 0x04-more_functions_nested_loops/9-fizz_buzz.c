710a916a1fb7278e91ffdcd2c9e3bf4134b99f31
YehiaElHadidi <hadidiyehia@gmail.com>
2023-03-23 10:40:40 -0700

    s

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - tests if input given is a digit
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz")
+		if (i != 100)
+			printf(" ");
+		if (!(i % 3 == 0 || i % 5 == 0))
+			printf("%d", i);
+	}
+}
