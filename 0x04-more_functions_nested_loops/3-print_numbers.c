b391dc70b622d7ce526a4ea59c6031307a0980d0
YehiaElHadidi <hadidiyehia@gmail.com>
2023-03-23 07:58:56 -0700

    s

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+/**
+ * print_numbers - tests if input given is a digit
+ * Return: void
+ * 
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for(i = 48; i < 48 + 9; i++)
+	{
+		_putchar(i);
+		_putchar('\n');
+	}
+}
