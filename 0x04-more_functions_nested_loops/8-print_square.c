a573dca24bbf79aee2e0bae254255df9a3aa7cb9
YehiaElHadidi <hadidiyehia@gmail.com>
2023-03-23 10:26:14 -0700

    s

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,13 +14,10 @@ void print_square(int size)
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
-		{
-			putchar('#');
-		}
-		putchar('\n');
+			_putchar('#');
+
+		_putchar('\n');
 	}
 	if (size <= 0)
-	{
-		putchar('\n');
-	}
+		_putchar('\n');
 }
