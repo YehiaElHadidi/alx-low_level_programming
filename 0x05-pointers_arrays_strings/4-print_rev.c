f6cf5b37a9cf9641b6c91b0a4c84ddd1504cbd2c
YehiaElHadidi <hadidiyehia@gmail.com>
2023-03-27 08:05:52 -0700

    s

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_rev - updates the value of a pointer to 98
+ * @s: a pointer
+ *
+ * Return: void ALWAYS
+ */
+void print_rev(char *s)
+{
+	int i, cnt;
+
+	cnt = 0;
+	while (s[cnt] != '\0')
+		cnt++;
+
+	for (i = cnt; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+}
