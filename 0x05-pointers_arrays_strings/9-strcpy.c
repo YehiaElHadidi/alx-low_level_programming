b0a48fe7d8c10ee3e12154bd9458f17025261ab1
YehiaElHadidi <hadidiyehia@gmail.com>
2023-03-27 08:58:07 -0700

    s

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcpy - updates the value of a pointer to 98
+ * @des: a pointer
+ * @src: ddd
+ * Return: char* ALWAYS
+ */
+char *_strcpy(char *des, char *src)
+{
+	int i, sz;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	sz = i;
+
+	for (i = 0; i <= sz; i++)
+	{
+		des[i] = src[i];
+	}
+	return (des);
+
+}
