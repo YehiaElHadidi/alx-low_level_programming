a397c00d46355898f10073f1e110f5ec0e79d5d9
YehiaElHadidi <hadidiyehia@gmail.com>
2023-03-27 07:39:08 -0700

    s

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+/**
+ * _strlen - updates the value of a pointer to 98
+ * @s: a pointer
+ *
+ * Return: int ALWAYS
+ */
+int _strlen(char *s)
+{
+	int cnt = 0;
+
+	while (s[cnt++] != '\0')
+
+	return (cnt);
+}
