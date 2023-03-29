13052ded93d2d68af0e9a7cc22ef60245a6efcf9
YehiaElHadidi <hadidiyehia@gmail.com>
2023-03-29 07:38:15 -0700

    s

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * _strcat - updates the value of a pointer to 98
+ * @des: a pointer
+ * @src: ss
+ * Return: char * ALWAYS
+ */
+char *_strcat(char *des, char *src)
+{
+	int i, sz, j, k;
+
+	i = 0;
+	while (des[i] != '\0')
+		i++;
+	sz = i - 1;
+
+	j = 0;
+	while (src[j] != '\0')
+		j++;
+	sz += j;
+
+	for (k = 0; k < j; k++)
+		temp[k + i] = src[k];
+
+}
