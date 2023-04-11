730bc47d64ae34a6e1c5a87745ea697f4fb75c13
YehiaElHadidi <hadidiyehia@gmail.com>
2023-04-11 05:45:29 -0700

    s

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * create_array - updates the value of a pointer to 98
+ * @size: a pointer
+ * @c: ss
+ *
+ * Return: char * ALWAYS
+ */
+char *create_array(unsigned int size, char c)
+{
+	int *ptr, i;
+	
+	ptr = ((char*)malloc(n * sizeof(char)));
+	for(i = 0; i < size; i++)
+		ptr[i] = c;
+	return (ptr);
