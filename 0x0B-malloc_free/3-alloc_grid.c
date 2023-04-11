73e909269591a36fda4e52c66670eff0c77cda4e
YehiaElHadidi <hadidiyehia@gmail.com>
2023-04-11 06:27:15 -0700

    s

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - updates the value of a pointer to 98
+ * @str: a pointer
+ * Return: char * ALWAYS
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **ptr;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+
+
+	ptr = (int **)malloc(height * sizeof(int));
+	if (ptr == NULL)
+                return (NULL);
+	for(i = 0; i < r1; i++){
+		ptr[i] = (int *)malloc(width * sizeof(int));
+	}
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			ptr[i][j] = 0;
+	return (ptr);
+}
