d7a0c69906fcbd4d7d6427b4c293e87b1599d8da
YehiaElHadidi <hadidiyehia@gmail.com>
2023-04-17 07:11:08 -0700

    s

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - prints a struct dog
+ * @d: struct dog to print
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
