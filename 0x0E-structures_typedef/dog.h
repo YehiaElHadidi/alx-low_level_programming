02f53b5f146049758db377c6d0dd39117c2c1bb8
YehiaElHadidi <hadidiyehia@gmail.com>
2023-04-17 06:51:15 -0700

    s

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
-#ifndef d
-#define d
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog - a dog's basic info
  * @name: First member
@@ -14,6 +15,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
 /**
  * dog_t - typedef for struct dog
  */
@@ -25,4 +27,5 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
+
 #endif
