94d82111e1b221f9069bb5bc3cc4dd9be67e787b
YehiaElHadidi <hadidiyehia@gmail.com>
2023-04-19 08:32:30 -0700

    s

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,18 +1,23 @@
+#include "function_pointers.h"
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
- * array_iterator - prints a name using another function
- * @array: name to print
- * @size: function to use to print name
- * @action: sdsdsd
- * Return: Void ALWAYS
+ * array_iterator - executes a function given on each element on an array
+ * @array: array to execute the function on each element of
+ * @size: size of the array
+ * @action: function to execute on each element of the array
+ *
+ * Return: void ALWAYS
  */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
+	size_t i = 0;
 
-	if (!name || size == 0 || !action)
+	if (!array || size == 0 || !action)
 		return;
+
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
