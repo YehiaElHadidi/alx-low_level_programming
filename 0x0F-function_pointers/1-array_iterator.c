a4f930a6f47ec36f3f4a4de28c3b370da2402950
YehiaElHadidi <hadidiyehia@gmail.com>
2023-04-19 08:30:42 -0700

    s

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,7 +10,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (!name || !action)
+	if (!name || size == 0 || !action)
 		return;
 	for (i = 0; i < size; i++)
 	{
